fd0f0abc90de3a97e87d5e87e84c97dbb5c321b9
Alex Harrison <alexjharrison@gmail.com>
2020-08-08 17:53:29 -0400

    Add project files.

---
+++ WpfApp1/Calculator.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace WpfApp1
+{
+    class Calculator
+    {
+        public string displayNumber;
+        public enum Operands { Add, Substract, Multiply, Divide }
+
+        private Nullable<Operands> operand;
+        private Nullable<double> firstNumber;
+        private Nullable<double> secondNumber;
+        private bool decimalPressed;
+
+        public Calculator()
+        {
+            this.displayNumber = "";
+            this.firstNumber = 0;
+            this.decimalPressed = false;
+            this.secondNumber = 0;
+            this.operand = null;
+        }
+
+        public void addNumber(double newNumber)
+        {
+            bool isFirstNumber = this.operand == null;
+
+            if (isFirstNumber && !this.decimalPressed)
+            {
+                this.firstNumber = (this.firstNumber * 10) + newNumber;
+                this.displayNumber = this.firstNumber.ToString();
+            } 
+
+            else if (isFirstNumber && this.decimalPressed)
+            {
+                int decimals = this.getDecimals((double)this.firstNumber);
+                this.firstNumber += newNumber * Math.Pow(10, -decimals);
+                this.firstNumber = Math.Round((double)this.firstNumber, decimals + 2);
+                this.displayNumber = this.firstNumber.ToString();
+            }
+
+            else if (!isFirstNumber && !this.decimalPressed)
+            {
+                this.secondNumber = (this.secondNumber * 10) + newNumber;
+                this.displayNumber = this.secondNumber.ToString();
+            }
+
+            else if (!isFirstNumber && this.decimalPressed)
+            {
+                int decimals = this.getDecimals((double)this.secondNumber);
+                this.secondNumber += newNumber * Math.Pow(10, -decimals);
+                this.secondNumber= Math.Round((double)this.secondNumber, decimals + 2);
+                this.displayNumber = this.secondNumber.ToString();
+            }
+            else
+            {
+                this.displayNumber = "Broken";
+            }
+            Console.WriteLine("stop");
+        }
+
+        public void addOperand(Operands operand)
+        {
+            this.operand = operand;
+            this.decimalPressed = false;
+        }
+
+        public void addPeriod()
+        {
+            this.decimalPressed = true;
+        }
+
+        public void calculate()
+        {
+            if (this.firstNumber == null || this.secondNumber == null) return;
+            double result = 0;
+            switch (this.operand) 
+            {
+                case Operands.Add:
+                    result = (double)(this.firstNumber + this.secondNumber);
+                    break;
+                case Operands.Substract:
+                    result = (double)(this.firstNumber - this.secondNumber);
+                    break;
+                case Operands.Multiply:
+                    result = (double)(this.firstNumber * this.secondNumber);
+                    break;
+                case Operands.Divide:
+                    result = (double)(this.firstNumber / this.secondNumber);
+                    break;
+            }
+            this.displayNumber = result.ToString();
+            this.reset();
+        }
+
+        public void reset()
+        {
+            this.firstNumber = 0;
+            this.secondNumber = 0;
+            this.operand = null;
+            this.decimalPressed = false;
+        }
+
+        public void clear()
+        {
+            this.reset();
+            this.displayNumber = "";
+        }
+
+        private int getDecimals(double argument)
+        {
+            if (!argument.ToString().Contains('.')) return 1;
+            return argument.ToString().Split(".")[1].Length + 1;
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculator calculator;
+        public MainWindow()
+        {
+            this.calculator = new Calculator();
+            InitializeComponent();
+        }
+
+
+        private void Operand_Click(object sender, RoutedEventArgs e)
+        {
+            string operand = getContent(sender);
+
+            if (operand == "+") this.calculator.addOperand(Calculator.Operands.Add);
+            if (operand == "-") this.calculator.addOperand(Calculator.Operands.Substract);
+            if (operand == "*") this.calculator.addOperand(Calculator.Operands.Multiply);
+            if (operand == "/") this.calculator.addOperand(Calculator.Operands.Divide);
+
+            this.text_result.Text = this.calculator.displayNumber;
+
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            this.calculator.clear();
+            this.text_result.Text = this.calculator.displayNumber;
+        }
+
+        private void Dot_Click(object sender, RoutedEventArgs e)
+        {
+            this.calculator.addPeriod();
+            this.text_result.Text = this.calculator.displayNumber;
+        }
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            double number = Convert.ToDouble(getContent(sender));
+            this.calculator.addNumber(number);
+            this.text_result.Text = this.calculator.displayNumber;
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            this.calculator.calculate();
+            this.text_result.Text = this.calculator.displayNumber;
+        }
+
+        private string getContent(object sender)
+        {
+            Button btn = sender as Button;
+            return btn.Content.ToString();
+        }
+    }
+}
